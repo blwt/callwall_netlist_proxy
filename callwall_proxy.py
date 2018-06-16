aadbfb9bd74ccf5405854c521a0870add7ad6e13
blwt <blwt@users.noreply.github.com>
2018-06-16 15:23:39 -0700

    Update callwall_proxy.py

--- callwall_proxy.py
+++ callwall_proxy.py
@@ -8,6 +8,7 @@ import SocketServer
 import SimpleHTTPServer
 import re
 import urllib
+import urllib2
 import urlparse
 import StringIO
 
@@ -17,26 +18,27 @@ class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         url = urlparse.urlparse(self.path)
         if url.netloc == 'www.whocalled.us':
-
             request = 'https://www.nomorobo.com/lookup/%s' % (url.query[url.query.rfind('=')+1:],)
-            response = urllib.urlopen(request)
 
-            if response.code == 404:
-                score=0
+            try:
+                response = urllib2.urlopen(request)
 
-            else:
-                match = re.findall("(?i)do not answer", response.read())
+                match = re.findall('(?i)do not answer', response.read())
                 if match:
                     score=11
                 else:
                     score=6
 
+            except urllib2.HTTPError, e:
+                score=0
+
             print '%s -> %s' % (request, score)
             self.copyfile(StringIO.StringIO('success=1&score=%d' % (score,)), self.wfile)
+
         else:
             self.copyfile(urllib.urlopen(self.path), self.wfile)
 
 if __name__ == '__main__':
     httpd = SocketServer.ForkingTCPServer(('', PORT), Proxy)
-    print "serving at port", PORT
+    print 'Listening at port: %d' % (PORT,)
     httpd.serve_forever()
